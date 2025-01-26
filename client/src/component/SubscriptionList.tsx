d2d0bd3d545a870c16c0450a78f6ae1c67ee2313
daviddeblas <71023594+daviddeblas@users.noreply.github.com>
2025-01-26 00:02:47 -0500

    [CLIENT] Adding the SubscriptionList component

---
+++ client/src/component/SubscriptionList.tsx
@@ -0,0 +1,43 @@
+import './SubscriptionList.css';
+
+type EmailData = {
+    brandName: string;
+    fromAddress: string;
+    subject?: string;
+    unsubscribeLink: string;
+};
+
+interface SubscriptionListProps {
+    emails: EmailData[];
+}
+
+function SubscriptionList({ emails }: SubscriptionListProps) {
+    if (!emails || emails.length === 0) {
+        return <p>No subscriptions found.</p>;
+    }
+
+    return (
+        <div className="subscription-list-container">
+            <h2>My Subscriptions</h2>
+            <ul className="subscription-list-ul">
+                {emails.map((sub, idx) => (
+                    <li key={idx} className="subscription-item">
+                        <div className="subscription-brand">{sub.brandName}</div>
+                        <div className="subscription-from">&lt;{sub.fromAddress}&gt;</div>
+                        {sub.subject && (
+                            <div className="subscription-subject">{sub.subject}</div>
+                        )}
+                        <button
+                            className="unsubscribe-btn"
+                            onClick={() => window.open(sub.unsubscribeLink, '_blank')}
+                        >
+                            Unsubscribe
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default SubscriptionList;
