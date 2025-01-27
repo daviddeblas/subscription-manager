0c60ba97a7a7c227c4f5695695b994b5da16816c
daviddeblas <71023594+daviddeblas@users.noreply.github.com>
2025-01-27 01:17:58 -0500

    Deplaced all the CORS logic in SecurityConfig

--- subscription-manager/src/main/java/com/subscription_manager/configuration/SecurityConfig.java
+++ subscription-manager/src/main/java/com/subscription_manager/configuration/SecurityConfig.java
@@ -6,26 +6,34 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.http.HttpStatus;
 
-/**
- * Configuration class for setting up CORS.
- *
- * This class defines the CORS mappings, specifying allowed origins, HTTP methods, headers, and credentials.
- */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                // Disable CSRF because we use REST APIs
+                .csrf(csrf -> csrf.disable())
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/emails").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/", "/index.html").permitAll()
                         .anyRequest().authenticated()
                 )
+                // TODO
+                // Local      : http://localhost:5173/subscriptions
+                // Production : https://subscription-manager-ten.vercel.app/subscriptions
                 .oauth2Login(oauth2 -> oauth2
                         .defaultSuccessUrl("http://localhost:5173/subscriptions", true)
+                )
+
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 );
 
         return http.build();
